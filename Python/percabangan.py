989e5968095c9301143f7e99f170b5f68bd8263a
MIRZA-KAYANG <mirzasyraf@gmail.com>
2025-10-15 12:12:05 +0700

    Update yang Error Tunjangan

--- Python/percabangan.py
+++ Python/percabangan.py
@@ -1,9 +1,10 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
-gaji = 300000*30
+gaji = 9000000
 lembur = 3500  # /jam
 Total = "gaji + tunjangan_jabatan + tunjangan_pendidikan + lembur"
 
+print('='*15, 'Input Data', '='*15)
 Nama_Karyawan = input("Masukkan Nama Karyawan : ")
 tunjangan_jabatan = input("Masukkan Golongan Jabatan (1/2/3) : ")
 pendidikan = input("Masukkan Pendidikan (SMA/D1/D3/S1) : ")
@@ -26,22 +27,23 @@ else:
     (0)
 
 # Kondisi Pendidikan
-if pendidikan == "SMA":
-    tunjangan_pendidikan = 5/100*gaji
-elif pendidikan == "D1":
-    tunjangan_pendidikan = 10/100*gaji
-elif pendidikan == "D3":
-    tunjangan_pendidikan = 20/100*gaji
-elif pendidikan == "S1":
-    tunjangan_pendidikan = 30/100*gaji
-else:(0)
+if pendidikan.upper() == "SMA":
+    tunjangan_pendidikan = 0.05 * gaji
+elif pendidikan.upper() == "D1":
+    tunjangan_pendidikan = 0.10 * gaji
+elif pendidikan.upper() == "D3":
+    tunjangan_pendidikan = 0.20 * gaji
+elif pendidikan.upper() == "S1":
+    tunjangan_pendidikan = 0.30 * gaji
+else:
+    (0)
+
+Total = gaji_pokok + tunjangan_jabatan + tunjangan_pendidikan + Uang_lembur
 
 print("===Struk Gaji Karyawan===")
 print(f"Nama Karyawan : {Nama_Karyawan}")
-print(f"Gaji Pokok : Rp. {gaji:,}")
-print(f"Tunjangan Jabatan : Rp. {tunjangan_jabatan:,}")
-print(f"Tunjangan Pendidikan : Rp. {tunjangan_pendidikan:,}")
-Total = int(gaji_pokok + tunjangan_jabatan + tunjangan_pendidikan + Uang_lembur)
+print(f"Gaji Pokok : Rp. {gaji:,.0f}")
+print(f"Tunjangan Jabatan : Rp. {tunjangan_jabatan:,.0f}")
+print(f"Tunjangan Pendidikan : Rp. {tunjangan_pendidikan:,.0f}")
 print(f"Total Gaji Bulan ini : Rp. {Total:,}")
 print('='*25)
-
