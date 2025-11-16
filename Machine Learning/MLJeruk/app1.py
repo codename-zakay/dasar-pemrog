cf50970c91a15b04c96ef934e692bbe906008fd0
MIRZA-KAYANG <145776926+codename-zakay@users.noreply.github.com>
2025-11-16 11:06:51 +0700

    FilePath

--- Machine Learning/MLJeruk/app1.py
+++ Machine Learning/MLJeruk/app1.py
@@ -7,7 +7,7 @@ st.set_page_config(
 	page_icon = ":tangerine:"
 )
 
-model = joblib.load("model_klasifikasi_jeruk.joblib")
+model = joblib.load("/Machine Learning/MLJeruk/model_klasifikasi_jeruk.joblib")
 
 st.title(":tangerine: Belajar Klasifikasi Jeruk")
 st.markdown("Aplikasi machine learning classification untuk memprediksi kualitas jeruk")
