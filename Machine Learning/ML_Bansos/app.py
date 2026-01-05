79a0ef45538ecea1e4a3d37f9c85678dea86e18f
MIRZA-KAYANG <mirzasyraf@gmail.com>
2026-01-05 08:11:40 +0700

    RF - V3

--- Machine Learning/ML_Bansos/app.py
+++ Machine Learning/ML_Bansos/app.py
@@ -609,6 +609,7 @@ if uploaded_file is not None:
                 
                 wilayah_stats['Belum_Dapat'] = wilayah_stats['Total'] - wilayah_stats['Sudah_Dapat']
                 wilayah_stats['Persentase_Belum'] = (wilayah_stats['Belum_Dapat'] / wilayah_stats['Total'] * 100)
+                wilayah_stats['Persentase_Belum_Display'] = wilayah_stats['Persentase_Belum'].apply(lambda x: f"{x:.1f}%")
                 
                 col1, col2 = st.columns(2)
                 with col1:
